21c3862985dba14156b4655c7edc90a645aab17d
code-masud <code.masud@hotmail.com>
2026-03-17 02:20:58 +0600

    Update main.py

--- linked-list/singly/project/main.py
+++ linked-list/singly/project/main.py
@@ -6,17 +6,19 @@ def main():
     l.insert_at_top('A')
     l.insert_at_top('B')
 
-    l.display()
-
-    l.delete_at_position(1)
-    # l.insert_at_position('D', 0)
-    l.display()
-
-    # l.delete_from_top()
-    # l.delete_from_top()
-
-    # l.insert_at_end('E')
     # l.display()
+
+    # l.delete_at_position(1)
+    # l.insert_at_position('D', 0)
+    # l.display()
+
+    # l.delete_from_top()
+    # l.delete_from_top()
+
+    l.insert_at_end('E')
+    l.display()
+    l.reverse()
+    l.display()
 
     # # l.insert_at_top('X')
     # # l.insert_at_top('Y')
