c9582038355ddec4b7d16edb15032b6f616bed43
code-masud <code.masud@hotmail.com>
2026-03-17 02:20:55 +0600

    Update linkedlist.py

--- linked-list/singly/project/modules/linkedlist.py
+++ linked-list/singly/project/modules/linkedlist.py
@@ -87,6 +87,18 @@ class LinkedList:
         if current.next:
             current.next = current.next.next
 
+    def reverse(self):
+        prev = None
+        current = self.top
+
+        while current:
+            next_node = current.next 
+            current.next = prev
+            prev = current
+            current = next_node
+        
+        self.top = prev
+
     def display(self):
         print('Stack:', end=' ')
         temp = self.top
